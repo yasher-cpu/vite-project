1ec9c6340b749035d19b48b3e2b2486be225a0e6
yasher-cpu <yashersantos02@gmail.com>
2025-02-06 18:58:07 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Mark Yasher Santos</h1>
+      <div className="box">CPEITEL</div>
+    </div>
+  );
+};
+
+export default App;
